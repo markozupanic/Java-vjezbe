f677ea462b3ff0bfebf6d96ef63a3610d2a9dcb0
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-06-21 14:08:35 +0200

    Vise dimenzionalni nizovi

---
+++ Edunova04/src/edunova04/E03ViseDimenzionalniNizovi.java
@@ -0,0 +1,33 @@
+package edunova04;
+
+public class E03ViseDimenzionalniNizovi {
+
+	public static void main(String[] args) {
+		
+		
+		int[][] matrica= new int[10][10];
+		
+		matrica[9][9]=7;
+		
+		System.out.println(matrica);
+		
+		int[][] tablica = {
+				{1,0,0},
+				{0,1,0},
+				{0,0,1}
+		};
+		
+		System.out.println(tablica);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
