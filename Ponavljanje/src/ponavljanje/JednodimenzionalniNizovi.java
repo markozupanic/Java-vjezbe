d669525204f3785d72150cc19fb2cd2dedf629cc
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-07-02 17:20:32 +0200

    JednodimenzionalniNizovi

---
+++ Ponavljanje/src/ponavljanje/JednodimenzionalniNizovi.java
@@ -0,0 +1,78 @@
+package ponavljanje;
+
+import java.util.Arrays;
+
+public class JednodimenzionalniNizovi {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		int t1=2;
+		int t2=-1;
+		
+		int [] temp;
+		
+		temp =new int[12];
+		
+		temp[0]=2;
+		temp[1]=-1;
+		
+		
+		temp[temp.length-1]=6;
+		
+		System.out.println(Arrays.toString(temp));
+		
+		int boje[]=new int[4];
+		boje[1]=7;
+		System.out.println(boje[1]);
+		
+		int[] vrijednosti= {2,3,3,2,3,8,5,6,7,5,5};
+		
+		System.out.println(vrijednosti[0] + "," +
+		vrijednosti[vrijednosti.length-1]);
+		
+		
+		System.out.print(vrijednosti[0]);
+		System.out.print(",");
+		System.out.print(vrijednosti[vrijednosti.length-1]);
+		System.out.println();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
