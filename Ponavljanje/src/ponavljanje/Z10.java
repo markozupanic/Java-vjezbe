07543373003d901f8c3b5078b255ea3d665bf53b
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-07-04 01:02:47 +0200

    Vjezbe

--- Ponavljanje/src/ponavljanje/Z10.java
+++ Ponavljanje/src/ponavljanje/Z10.java
@@ -6,22 +6,22 @@ public class Z10 {
 		// cjelih brojeva program ispisuje 
 		// najveæi
 		
-		public static void main(String[] args) {
-			
-			int[] niz = {5,5,8,6,-6,8,-1,0,9,0};
-			// 9
-			
-			
-			int max = Integer.MIN_VALUE;
-			for (int a : niz) {
-				if (a > max) {
-					max = a;
-				}
+	
 
+	
+
+	public static void main(String[] args) {
+
+		int[] niz = { 5, 5, 8, 6, -6, 8, -1, 0, 9, 0 };
+
+		int max = Integer.MIN_VALUE;
+		for (int a : niz) {
+			if (a > max) {
+				max = a;
 			}
 
-			System.out.println(max);
+		}
 
+		System.out.println(max);
 	}
-
 }
