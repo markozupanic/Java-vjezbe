ffa5d6d820cd044dac4c1f636af01d73fd3d977a
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-07-04 15:07:21 +0200

    Z02

---
+++ Edunova05/src/edunova/Z02.java
@@ -0,0 +1,26 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z02 {
+
+	public static void main(String[] args) {
+		// Program od korisnika unosi
+		// cjele brojeve dok korisnik
+		// ne unese broj -1
+		// program ispisuje sumu svih unesenih
+		// brojeva
+		// koristeæi while
+		
+		int i=0,suma=0;
+		
+		while(i!=-1) {
+			i=Integer.parseInt(JOptionPane.
+					showInputDialog("broj"));
+			suma+=i;
+		}
+
+		System.out.println(suma);
+	}
+
+}
