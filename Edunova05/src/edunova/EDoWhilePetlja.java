be8c198112a4e3915c6cd21e352c04ba926c97b9
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-07-04 14:41:13 +0200

    Do while petlja

---
+++ Edunova05/src/edunova/EDoWhilePetlja.java
@@ -0,0 +1,21 @@
+package edunova;
+
+public class EDoWhilePetlja {
+
+	public static void main(String[] args) {
+		
+				int i=10;
+				do {
+					System.out.println("Osijek");
+				}while(i>10);
+				
+				
+				do {
+					//break;
+				}while(true);
+		
+		
+		
+	}
+
+}
