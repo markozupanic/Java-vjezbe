79618c24f86996b924b6be47c39a830810cdd19c
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-07-04 14:36:47 +0200

    While petlja

---
+++ Edunova05/src/edunova/WhilePetlja.java
@@ -0,0 +1,70 @@
+package edunova;
+
+public class WhilePetlja {
+
+	public static void main(String[] args) {
+		
+		while(true) {
+			break;
+		}
+		int i=0;
+		while(i<10) {
+			if(i+1==2) {
+				i++;
+				continue;
+			}
+			System.out.println(++i);
+		}
+		
+		
+		for(int t=10;t>10;t++) {
+			System.out.println("Osijek");
+		}
+		
+		int t=2;
+		while(t>2) {
+			System.out.println("Edunova");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
