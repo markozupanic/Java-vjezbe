e3450b3e62157b6886a73d17152c46dd412ba65f
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-07-19 18:48:35 +0200

    Z05

---
+++ Vjezbanje/src/vjezbanje3/Z05.java
@@ -0,0 +1,35 @@
+package vjezbanje3;
+
+import javax.swing.JOptionPane;
+
+public class Z05 {
+
+	public static void main(String[] args) {
+		// program unosi broj izmeðu 
+		// 1 i 999
+		// U sluèaju da je izvan tog raspona
+		// ispisati grešku i prekinuti program
+		// Program ispisuje 1. znamenku upisanog broja
+		
+		// -5 greška
+		// 1067 greška
+		// 456 4
+		// 6 6
+		// 89 8
+		
+		
+		int i=Integer.parseInt(JOptionPane.showInputDialog("x"));
+		
+		if(i<1 || i>1000) {
+			System.out.println("Greska");
+		}else if( i<10) {
+			System.out.println(i);
+		}else if(i>=10 && i<100) {
+			System.out.println(i/10);
+		}else{
+			System.out.println(i/100);
+		}
+
+	}
+
+}
