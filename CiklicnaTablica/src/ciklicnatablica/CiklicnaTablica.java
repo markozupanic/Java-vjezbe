77bde8d7cb01de9a0cf5c6f5de53f39f4a2c1736
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-08-08 23:45:57 +0200

    tablica

--- CiklicnaTablica/src/ciklicnatablica/CiklicnaTablica.java
+++ CiklicnaTablica/src/ciklicnatablica/CiklicnaTablica.java
@@ -1,5 +1,7 @@
 package ciklicnatablica;
 
+import java.lang.reflect.Array;
+
 import javax.swing.JOptionPane;
 
 public class CiklicnaTablica {
@@ -13,19 +15,20 @@ public class CiklicnaTablica {
 		
 		int[][] tablica=new int[red][stupac];
 		
+		int umnozak=red * stupac;
 		
-		int pocetniRed=red;
-		int prviDesniStupac=stupac-1;
+		int pocetniRed=red-1;
+		int prviLjeviStupac=stupac;
 		
 		
 		
 		
 		
 		for(int i=0;i<pocetniRed;i++) {
-			System.out.printf("%5d", (i+1));
+			System.out.printf("%4d", (i+1));
 		}
 		
-		for(int j=0;j<prviDesniStupac;j++) {
+		for(int j=pocetniRed;j<prviLjeviStupac;j++) {
 			System.out.printf("%4d",(j+1));
 		
 		System.out.println();
@@ -50,7 +53,7 @@ public class CiklicnaTablica {
 			/*for(int i=0;i<red;i++) {
 				for(int j=0;j<stupac;j++) {
 					System.out.printf("%5d", (i+1)*(j+1));
-					
+					(j+1)
 				}
 				System.out.println();
 		}
