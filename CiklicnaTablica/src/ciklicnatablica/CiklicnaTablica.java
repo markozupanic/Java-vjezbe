8423c2e336eb911356c8a5650f892b8102ab8a54
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-08-22 14:35:34 +0200

    Ciklična tablica

--- CiklicnaTablica/src/ciklicnatablica/CiklicnaTablica.java
+++ CiklicnaTablica/src/ciklicnatablica/CiklicnaTablica.java
@@ -1,37 +1,61 @@
 package ciklicnatablica;
 
-import java.lang.reflect.Array;
+
 
 import javax.swing.JOptionPane;
 
 public class CiklicnaTablica {
 	
+	   int red=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+	    int stupac=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));;
+       int[][] tablica = new int[red][stupac];
+       int brojac=1;
+       char smjer='E';
+       int ljevo=0, gore=0, desno=stupac-1, dolje=red-1;
+       while(ljevo<=desno && gore<=dolje) {
+           if(smjer == 'E') {
+               for(int i=ljevo; i<=desno; i++) {
+                   tablica[gore][i] = brojac++;
+               }
+               gore++;
+               smjer='S';
+           }
+           else if(smjer=='S') {
+               for(int i=gore; i<=dolje; i++) {
+                   tablica[i][desno] = brojac++;
+               }
+               desno--;
+               smjer='W';
+           }
+           else if(smjer=='W') {
+               for(int i=desno; i>=ljevo; i--) {
+                   tablica[dolje][i] = brojac++;
+               }
+               dolje--;
+               smjer='N';
+           }
+           else {
+               for(int i=dolje; i>=gore; i--) {
+                   tablica[i][ljevo] = brojac++;
+               }
+               ljevo++;
+               smjer='E';
+           }
+       }
+       for (int i = 0; i < tablica.length; i++) {
+			for (int j = 0; j < tablica[i].length; j++) {
+
+				System.out.printf("%4d", tablica[i][j]);
+			}
+			System.out.println();
+		}
+
+   }
 	
-	public static void main(String[] args) {
-		
-		int red=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
-		int stupac=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
-		
-		
-		int[][] tablica=new int[red][stupac];
-		
-		int umnozak=red * stupac;
-		
-		int pocetniRed=red-1;
-		int prviLjeviStupac=stupac;
-		
-		
-		
-		
-		
-		for(int i=0;i<pocetniRed;i++) {
-			System.out.printf("%4d", (i+1));
-		}
-		
-		for(int j=pocetniRed;j<prviLjeviStupac;j++) {
-			System.out.printf("%4d",(j+1));
-		
-		System.out.println();
+}
+
+	
+
 		
 		
 		
@@ -50,17 +74,10 @@ public class CiklicnaTablica {
 		
 		
 		
-			/*for(int i=0;i<red;i++) {
-				for(int j=0;j<stupac;j++) {
-					System.out.printf("%5d", (i+1)*(j+1));
-					(j+1)
-				}
-				System.out.println();
-		}
-		*/
 		
 		
-	}
+		
+	
 
 	}
-}
+
