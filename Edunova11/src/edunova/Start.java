929455934b5cf7589615b71dabbfbae0a4c6c9ed
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-08-23 16:08:17 +0200

    Start

--- Edunova11/src/edunova/Start.java
+++ Edunova11/src/edunova/Start.java
@@ -1,12 +1,93 @@
 package edunova;
 
+import java.util.Scanner;
+
+import edunovautil.Pomocno;
+
 public class Start {
 	
 	public Start() {
+		Pomocno.ulaz=new Scanner(System.in);
+		System.out.println("***** Edunova konzolna aplikacija v1 *****");
+		izbornik();
+		Pomocno.ulaz.close();
+		
 		
 	}
 
+	private void pokreniProgram() {
+		switch (Pomocno.ucitajInt("Odaberi program", 1, 5)) {
+		case 1:
+			smjerovi();
+			break;
+			
+		case 2:
+			grupe();
+			break;	
+			
+		case 3:
+			polaznici();
+			break;	
+			
+		case 4:
+			predavaci();
+			break;	
+			
+		case 5:
+			System.out.println("Doviðenja");
+		
+		}
+		
+	}
 	
+	
+	private void izbornik() {
+		System.out.println("");
+		System.out.println("Glavni izbornik");
+		System.out.println("1. Smjerovi");
+		System.out.println("2. Grupe");
+		System.out.println("3. Polaznici");
+		System.out.println("4. Predavaèi");
+		System.out.println("5. Izlaz");
+		System.out.println("");
+		pokreniProgram(); 
+		
+	}
+
+
+	
+
+
+	private void predavaci() {
+		System.out.println("Doðe izbornik za predavaèe");
+		//za sada
+		izbornik();
+		
+	}
+
+
+	private void polaznici() {
+		System.out.println("Doðe izbornik za predavaèe");
+		//za sada
+		izbornik();
+		
+	}
+
+
+	private void grupe() {
+		System.out.println("Doðe izbornik za predavaèe");
+		//za sada
+		izbornik();
+		
+	}
+
+
+	private void smjerovi() {
+		System.out.println("Doðe izbornik za smjerove");
+		izbornik();
+	}
+
+
 	public static void main(String[] args) {
 		new Start();
 	}
---
+++ Edunova11/src/edunovautil/Pomocno.java
@@ -0,0 +1,83 @@
+package edunovautil;
+
+import java.util.Scanner;
+
+public class Pomocno {
+	
+	public static Scanner ulaz;
+
+	/**
+	 * Metoda osigurava unos cjelog broja
+	 * koji može biti u cijelom rasponu
+	 * int tip podatka
+	 * @param poruka Koja se prikazuje korisniku
+	 * @return Uèitani cjeli broj
+	 */
+	public static int ucitajInt(String poruka) {
+		while(true) {
+			System.out.print(poruka + ": ");
+			try {
+				return Integer.parseInt(ulaz.nextLine());
+			} catch (Exception e) {
+				System.out.println("Niste unijeli cijeli broj");
+			}
+		}
+	}
+	
+	public static int ucitajInt(String poruka, boolean pozitivni) {
+		int i = ucitajInt(poruka);
+		if(!pozitivni) {
+			return i;
+		}
+		while(true) {
+			if(i<0) {
+				System.out.println(
+						"Uèitani broj mora biti pozitivan");
+				i=ucitajInt(poruka);
+				continue;
+			}
+			return i;
+		}
+	}
+	
+	public static int ucitajInt(String poruka, 
+			int min, int max) {
+		int i = ucitajInt(poruka);
+		while(true) {
+			if(i>=min && i<=max) {
+				return i;
+			}else {
+				System.out.println( 
+						"Broj mora biti izmeðu " + 
+				min + " " + max);
+				i=ucitajInt(poruka);
+			}
+		}
+	}
+	
+	public static String ucitajString(String poruka) {
+		String s;
+		while(true) {
+			System.out.print(poruka + ": ");
+			s = ulaz.nextLine();
+			if(s.trim().isEmpty()) {
+				System.out.println( 
+						"Obavezan unos");
+				continue;
+			}
+			return s;
+		}
+	}
+	
+	
+	
+	/**
+	 * https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public static int slucajniBroj(int min, int max) {
+		return min + (int)(Math.random() * ((max - min) + 1));
+	}
+}
