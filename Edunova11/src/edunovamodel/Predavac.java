baed0fc7276fe79a7353bbd6964cf9ca0f8bfae2
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-08-23 15:48:08 +0200

    Smjer

--- Edunova11/src/edunovamodel/Predavac.java
+++ Edunova11/src/edunovamodel/Predavac.java
@@ -1,5 +1,17 @@
 package edunovamodel;
 
-public class Predavac {
+public class Predavac extends Osoba {
+	
+	private String iban;
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+	
+	
 
 }
