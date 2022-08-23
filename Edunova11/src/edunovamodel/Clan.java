3f3553f2a010ec63ea33ce37aa8a3810b8a8c4cd
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-08-23 15:53:35 +0200

    Grupa

---
+++ Edunova11/src/edunovamodel/Clan.java
@@ -0,0 +1,22 @@
+package edunovamodel;
+
+public class Clan extends Entitet{
+	
+	private Grupa grupa;
+	private Polaznik polaznik;
+	public Grupa getGrupa() {
+		return grupa;
+	}
+	public void setGrupa(Grupa grupa) {
+		this.grupa = grupa;
+	}
+	public Polaznik getPolaznik() {
+		return polaznik;
+	}
+	public void setPolaznik(Polaznik polaznik) {
+		this.polaznik = polaznik;
+	}
+	
+	
+
+}
---
+++ Edunova11/src/edunovamodel/Grupa.java
@@ -0,0 +1,68 @@
+package edunovamodel;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Grupa extends Entitet{
+	
+	private String naziv;
+	private Smjer smjer;
+	private Date datumPocetka;
+	private Integer maksimalnoPolaznika;
+	private Predavac predavac;
+	
+	private List<Clan> clanovi =new ArrayList<>();
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public Smjer getSmjer() {
+		return smjer;
+	}
+
+	public void setSmjer(Smjer smjer) {
+		this.smjer = smjer;
+	}
+
+	public Date getDatumPocetka() {
+		return datumPocetka;
+	}
+
+	public void setDatumPocetka(Date datumPocetka) {
+		this.datumPocetka = datumPocetka;
+	}
+
+	public Integer getMaksimalnoPolaznika() {
+		return maksimalnoPolaznika;
+	}
+
+	public void setMaksimalnoPolaznika(Integer maksimalnoPolaznika) {
+		this.maksimalnoPolaznika = maksimalnoPolaznika;
+	}
+
+	public Predavac getPredavac() {
+		return predavac;
+	}
+
+	public void setPredavac(Predavac predavac) {
+		this.predavac = predavac;
+	}
+
+	public List<Clan> getClanovi() {
+		return clanovi;
+	}
+
+	public void setClanovi(List<Clan> clanovi) {
+		this.clanovi = clanovi;
+	}
+	
+	
+	
+
+}
