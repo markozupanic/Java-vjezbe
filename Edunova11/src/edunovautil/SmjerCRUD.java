71b7d2f2a498f3d31e70ddead376f1b6e644edcf
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-09-01 17:17:52 +0200

    CRUD

--- Edunova11/src/edunovautil/SmjerCRUD.java
+++ Edunova11/src/edunovautil/SmjerCRUD.java
@@ -1,5 +1,39 @@
 package edunovautil;
 
-public class SmjerCRUD {
+import java.util.List;
 
+import edunovamodel.Smjer;
+
+public class SmjerCRUD {
+	
+	public static Smjer unosNovog() {
+		Smjer s=new Smjer();
+		s.setSifra(Pomocno.ucitajInt("Unesite sifru smjera (pozitivni broj)",true));
+		s.setNaziv(Pomocno.ucitajString("Unesi naziv smjera"));
+		
+		return s;
+	}
+	
+	public static void ispis(List<Smjer> smjerovi) {
+		
+		int rb=1;
+		System.out.println("");
+		System.out.println("Smjerovi u aplikaciji");
+		for(Smjer s:smjerovi) {
+			
+			System.out.println(rb++ + ". " + s.getNaziv());
+			
+		}
+		System.out.println("--------------");
+		
+		
+	}
+
+	
+	public static void promjena(Smjer s) {
+		s.setSifra(Pomocno.ucitajInt("šifra (" 
+				+ s.getSifra() + ")", true));
+		s.setNaziv(Pomocno.ucitajString("naziv ( " 
+				+ s.getNaziv() + ")"));
+	}
 }
